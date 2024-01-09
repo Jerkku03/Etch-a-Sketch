d4809859f8f309d294e2cc5bf4759b8ecff0c543
Jerkku03 <jere.hietikko@live.fi>
2024-01-09 15:49:55 +0200

    paranna toimivuutta poista numero div

--- java.js
+++ java.js
@@ -3,24 +3,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const container = document.getElementById('container');
 
-divGrid(16, 16);
+const pixeli = document.getElementById('pixeli');
 
-function divGrid(rivi, rivi2) {
-    for (c = 0; c < (rivi * rivi2); c++) {
+
+
+
+
+function divGrid(koko) {
+    container.style.gridTemplateColumns = `repeat(${koko}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${koko}, 1fr)`
+
+    for (c = 0; c < (koko * koko); c++) {
         let cell = document.createElement("div");
-        cell.style.cssText = 'background-color: black;'
-
-        cell.innerText = (c + 1);
-        container.appendChild(cell);
+        cell.style.cssText = 'background-color: black; min-height: 5px; min-width:5px;'
         cell.addEventListener('mouseover', () => {
             cell.style.backgroundColor = 'white';
         }) 
-        cell.addEventListener('mouseout', () => {
-            cell.style.backgroundColor = 'green';
+        cell.addEventListener('mousedwn', () => {
+            cell.style.backgroundColor = '';
         }) 
+        container.appendChild(cell);
     };
 };
+pixeli.addEventListener('click', () => {
+    input = prompt('give input 0-100');
+    if (0 < input <= 100){
+    divGrid(input)
+    }
 
-
+});
 
 });
